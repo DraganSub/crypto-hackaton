b0c690bdf53e842134d7d39026374cc29142bb2c
davorpejdo <pejdoo2525@gmail.com>
2023-01-21 15:17:30 +0100

    coinapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 import { MainLayout } from "./layouts";
 function App() {
-  return (
-    <MainLayout />
-  );
+  return <MainLayout />;
 }
 
 export default App;
