0e96baf41dc69484f0ad2baaf73f161cc7f861c7
davorpejdo <pejdoo2525@gmail.com>
2023-01-21 15:17:30 +0100

    coinapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useGetCoinsQuery } from "./services";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { data, isLoading, error } = useGetCoinsQuery("");
+
+    return (
+        <div className="App">
+            {error ? (
+                <>{error}</>
+            ) : isLoading ? (
+                <>Loading...</>
+            ) : data ? (
+                <>
+                    {data.coins.map((val) => (
+                        <p key={val.name}>{val.name}</p>
+                    ))}
+                </>
+            ) : null}
+        </div>
+    );
 }
 
 export default App;
