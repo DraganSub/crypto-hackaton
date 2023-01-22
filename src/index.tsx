b63787d57a16498e7745da04c9b2e53612946389
davorpejdo <pejdoo2525@gmail.com>
2023-01-22 11:11:32 +0100

    rebase on develpo

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./stores";
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
-=======
->>>>>>> origin/redux
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
-<<<<<<< HEAD
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
@@ -22,11 +18,6 @@ root.render(
             </Provider>
         </BrowserRouter>
     </React.StrictMode>
-=======
-    <Provider store={store}>
-        <App />
-    </Provider>
->>>>>>> origin/redux
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,17 +15,6 @@ export default function HomePage() {
                         </div>
                         <div className="hero__data">22,988</div>
                     </div>
-    return (
-        <div className="homepage">
-            <section className="hero_section">
-                <h2 className="hero-title">Global Crypto Stats</h2>
-                <div className="hero_info_container">
-                    <div className="hero__total">
-                        <div className="hero__label">
-                            Total Cryptocurrencies
-                        </div>
-                        <div className="hero__data">22,988</div>
-                    </div>
 
                     <div className="hero__total">
                         <div className="hero__label">Total Exchanges</div>
@@ -43,305 +32,30 @@ export default function HomePage() {
                     </div>
                 </div>
 
+                <section className="crypto-currency">
+                    <div className="crypto__title-group">
+                        <h2 className="crypto-currency__title">
+                            Top 10 Cryptocurrencies
+                        </h2>
+                        <a className="show-more-btn">Show more</a>
+                    </div>
+                    <div className="crypto__grid">
+                        <CryptoList />
+                    </div>
+                </section>
 
-            <section className="crypto-currency">
-                <div className="crypto__title-group">
-                    <h2 className="crypto-currency__title">
-                        Top 10 Cryptocurrencies
-                    </h2>
-                    <a className="show-more-btn">Show more</a>
-                </div>
-                <div className="crypto__grid">
-                    <CryptoList />
-                </div>
+                <section className="crypto-news">
+                    <div className="crypto__title-group">
+                        <h2 className="crypto-news__title">
+                            Latest Crypto News
+                        </h2>
+                        <a className="show-more-btn">Show more</a>
+                    </div>
+                    <div className="crypto__grid__lg">{<NewsList />}</div>
+                </section>
             </section>
-
-            <section className="crypto-news">
-                <div className="crypto__title-group">
-                    <h2 className="crypto-news__title">Latest Crypto News</h2>
-                    <a className="show-more-btn">Show more</a>
-                </div>
-                <div className="crypto__grid__lg">{<NewsList />}</div>
-            </section>
         </div>
-      </section>
-
-      <section className="crypto-currency">
-        <div className="crypto__title-group">
-          <h2 className="crypto-currency__title">Top 10 Cryptocurrencies</h2>
-          <Link to="/Cryptocurrencies" className="show-more-btn">Show more</Link>
-        </div>
-        <div className="crypto__grid">
-          <Card title="1. Bitcoin" extra={<CryptoImg />} >
-            <div className="crypto__data">
-              <div className="crypto__card">
-                <div className="card__label">Price:</div>
-                <div className="card__value">$23.2K</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Market Cap:</div>
-                <div className="card__value">445.4B</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Daily Change:</div>
-                <div className="card__value">9.65%</div>
-              </div>
-            </div>
-          </Card>
-
-          <Card title="1. Bitcoin" extra={<CryptoImg />} >
-            <div className="crypto__data">
-              <div className="crypto__card">
-                <div className="card__label">Price:</div>
-                <div className="card__value">$23.2K</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Market Cap:</div>
-                <div className="card__value">445.4B</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Daily Change:</div>
-                <div className="card__value">9.65%</div>
-              </div>
-            </div>
-          </Card>
-
-          <Card title="1. Bitcoin" extra={<CryptoImg />} >
-            <div className="crypto__data">
-              <div className="crypto__card">
-                <div className="card__label">Price:</div>
-                <div className="card__value">$23.2K</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Market Cap:</div>
-                <div className="card__value">445.4B</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Daily Change:</div>
-                <div className="card__value">9.65%</div>
-              </div>
-            </div>
-          </Card>
-          <Card title="1. Bitcoin" extra={<CryptoImg />} >
-            <div className="crypto__data">
-              <div className="crypto__card">
-                <div className="card__label">Price:</div>
-                <div className="card__value">$23.2K</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Market Cap:</div>
-                <div className="card__value">445.4B</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Daily Change:</div>
-                <div className="card__value">9.65%</div>
-              </div>
-            </div>
-          </Card>
-          <Card title="1. Bitcoin" extra={<CryptoImg />} >
-            <div className="crypto__data">
-              <div className="crypto__card">
-                <div className="card__label">Price:</div>
-                <div className="card__value">$23.2K</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Market Cap:</div>
-                <div className="card__value">445.4B</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Daily Change:</div>
-                <div className="card__value">9.65%</div>
-              </div>
-            </div>
-          </Card>
-          <Card title="1. Bitcoin" extra={<CryptoImg />} >
-            <div className="crypto__data">
-              <div className="crypto__card">
-                <div className="card__label">Price:</div>
-                <div className="card__value">$23.2K</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Market Cap:</div>
-                <div className="card__value">445.4B</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Daily Change:</div>
-                <div className="card__value">9.65%</div>
-              </div>
-            </div>
-          </Card>
-          <Card title="1. Bitcoin" extra={<CryptoImg />} >
-            <div className="crypto__data">
-              <div className="crypto__card">
-                <div className="card__label">Price:</div>
-                <div className="card__value">$23.2K</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Market Cap:</div>
-                <div className="card__value">445.4B</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Daily Change:</div>
-                <div className="card__value">9.65%</div>
-              </div>
-            </div>
-          </Card>
-          <Card title="1. Bitcoin" extra={<CryptoImg />} >
-            <div className="crypto__data">
-              <div className="crypto__card">
-                <div className="card__label">Price:</div>
-                <div className="card__value">$23.2K</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Market Cap:</div>
-                <div className="card__value">445.4B</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Daily Change:</div>
-                <div className="card__value">9.65%</div>
-              </div>
-            </div>
-          </Card>
-          <Card title="1. Bitcoin" extra={<CryptoImg />} >
-            <div className="crypto__data">
-              <div className="crypto__card">
-                <div className="card__label">Price:</div>
-                <div className="card__value">$23.2K</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Market Cap:</div>
-                <div className="card__value">445.4B</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Daily Change:</div>
-                <div className="card__value">9.65%</div>
-              </div>
-            </div>
-          </Card>
-          <Card title="1. Bitcoin" extra={<CryptoImg />} >
-            <div className="crypto__data">
-              <div className="crypto__card">
-                <div className="card__label">Price:</div>
-                <div className="card__value">$23.2K</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Market Cap:</div>
-                <div className="card__value">445.4B</div>
-              </div>
-              <div className="crypto__card">
-                <div className="card__label">Daily Change:</div>
-                <div className="card__value">9.65%</div>
-              </div>
-            </div>
-          </Card>
-        </div>
-      </section>
-
-      <section className="crypto-news">
-        <div className="crypto__title-group">
-          <h2 className="crypto-news__title">Latest Crypto News</h2>
-          <Link to="/News" className="show-more-btn">Show more</Link>
-        </div>
-        <div className="crypto__grid__lg">
-          <Card className="crypto_news-card">
-            <div className="crypto__card__title">
-              <h5>What next for French victims of the FTX cryptocurrency exchange collapse?</h5>
-              <CryptoNewsImg />
-            </div>
-            <p>The collapse of the FTX cryptocurrency exchange has had repercussions around the globe, with more than a million clients losing money overni...</p>
-            <div className="news-info">
-              <p className="news-provider">newsbytesapp.com</p>
-              <p className="news-time">9 hours ago</p>
-            </div>
-          </Card>
-
-          <Card className="crypto_news-card">
-            <div className="crypto__card__title">
-              <h5>What next for French victims of the FTX cryptocurrency exchange collapse?</h5>
-              <CryptoNewsImg />
-            </div>
-            <p>The collapse of the FTX cryptocurrency exchange has had repercussions around the globe, with more than a million clients losing money overni...</p>
-            <div className="news-info">
-              <p className="news-provider">newsbytesapp.com</p>
-              <p className="news-time">9 hours ago</p>
-            </div>
-          </Card>
-
-          <Card className="crypto_news-card">
-            <div className="crypto__card__title">
-              <h5>What next for French victims of the FTX cryptocurrency exchange collapse?</h5>
-              <CryptoNewsImg />
-            </div>
-            <p>The collapse of the FTX cryptocurrency exchange has had repercussions around the globe, with more than a million clients losing money overni...</p>
-            <div className="news-info">
-              <p className="news-provider">newsbytesapp.com</p>
-              <p className="news-time">9 hours ago</p>
-            </div>
-          </Card>
-
-          <Card className="crypto_news-card">
-            <div className="crypto__card__title">
-              <h5>What next for French victims of the FTX cryptocurrency exchange collapse?</h5>
-              <CryptoNewsImg />
-            </div>
-            <p>The collapse of the FTX cryptocurrency exchange has had repercussions around the globe, with more than a million clients losing money overni...</p>
-            <div className="news-info">
-              <p className="news-provider">newsbytesapp.com</p>
-              <p className="news-time">9 hours ago</p>
-            </div>
-          </Card>
-
-          <Card className="crypto_news-card">
-            <div className="crypto__card__title">
-              <h5>What next for French victims of the FTX cryptocurrency exchange collapse?</h5>
-              <CryptoNewsImg />
-            </div>
-            <p>The collapse of the FTX cryptocurrency exchange has had repercussions around the globe, with more than a million clients losing money overni...</p>
-            <div className="news-info">
-              <p className="news-provider">newsbytesapp.com</p>
-              <p className="news-time">9 hours ago</p>
-            </div>
-          </Card>
-
-          <Card className="crypto_news-card">
-            <div className="crypto__card__title">
-              <h5>What next for French victims of the FTX cryptocurrency exchange collapse?</h5>
-              <CryptoNewsImg />
-            </div>
-            <p>The collapse of the FTX cryptocurrency exchange has had repercussions around the globe, with more than a million clients losing money overni...</p>
-            <div className="news-info">
-              <p className="news-provider">newsbytesapp.com</p>
-              <p className="news-time">9 hours ago</p>
-            </div>
-          </Card>
-
-          <Card className="crypto_news-card">
-            <div className="crypto__card__title">
-              <h5>What next for French victims of the FTX cryptocurrency exchange collapse?</h5>
-              <CryptoNewsImg />
-            </div>
-            <p>The collapse of the FTX cryptocurrency exchange has had repercussions around the globe, with more than a million clients losing money overni...</p>
-            <div className="news-info">
-              <p className="news-provider">newsbytesapp.com</p>
-              <p className="news-time">9 hours ago</p>
-            </div>
-          </Card>
-
-          <Card className="crypto_news-card">
-            <div className="crypto__card__title">
-              <h5>What next for French victims of the FTX cryptocurrency exchange collapse?</h5>
-              <CryptoNewsImg />
-            </div>
-            <p>The collapse of the FTX cryptocurrency exchange has had repercussions around the globe, with more than a million clients losing money overni...</p>
-            <div className="news-info">
-              <p className="news-provider">newsbytesapp.com</p>
-              <p className="news-time">9 hours ago</p>
-            </div>
-          </Card>
-        </div>
-      </section>
-    </div>
-  )
+    );
 }
 
 const CryptoImg = () => {
