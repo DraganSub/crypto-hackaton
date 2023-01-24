46130836b7485112df30754f8e02a27cd5ac4888
dsubotic <dragan.subi7@gmail.com>
2023-01-24 21:25:54 +0100

    add base news page mockup

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,5 +1,40 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Select } from "antd";
+import { NewsList } from "../components";
+import { coins } from "../utility";
+const { Option } = Select;
 export default function News() {
-  return <div> News page </div>
+  const [searchInputValue, setSearchInputValue] = useState<string>("")
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const onChange = (value: string) => {
+    setSearchInputValue(value);
+  };
+
+
+  const onSearch = (value: string) => {
+    setSearchTerm(value)
+  };
+
+  return <section className="crypto-news">
+    <div className="crypto__title-group">
+      <Select
+        className="crypto__search-field"
+        showSearch
+        placeholder="Enter your crypto here..."
+        optionFilterProp="children"
+        onChange={onChange}
+        onSearch={onSearch}
+        filterOption={(input, option) =>
+          (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+        }
+        options={coins}
+      />
+
+
+    </div>
+    <div className="crypto__grid__lg">
+      {<NewsList limit="6" />}
+    </div>
+  </section>
 }
--- src/utility/coins.ts
+++ src/utility/coins.ts
@@ -1,102 +1,406 @@
-export const coins = [
-    "Bitcoin",
-    "Ethereum",
-    "Tether USD",
-    "USDC",
-    "BNB",
-    "XRP",
-    "Binance USD",
-    "Cardano",
-    "Dogecoin",
-    "OKB",
-    "Solana",
-    "Polygon",
-    "Polkadot",
-    "Shiba Inu",
-    "Chainlink",
-    "Wrapped Ether",
-    "Litecoin",
-    "TRON",
-    "Uniswap",
-    "Dai",
-    "Avalanche",
-    "Wrapped BTC",
-    "Cosmos",
-    "Toncoin",
-    "Monero",
-    "Ethereum Classic",
-    "Lido DAO Token",
-    "Huobi Token",
-    "Bitcoin Cash",
-    "Frax Share",
-    "Trust Wallet Token",
-    "Stellar",
-    "Algorand",
-    "Hedera",
-    "Aptos",
-    "Filecoin",
-    "The Sandbox",
-    "Wrapped liquid staked Ether 2.0",
-    "Decentraland",
-    "Internet Computer (DFINITY)",
-    "NEAR Protocol",
-    "VeChain",
-    "EnergySwap",
-    "Frax",
-    "Quant",
-    "PancakeSwap",
-    "Immutable X",
-    "Cronos",
-    "Aave",
-    "Terra Classic",
-    "Flow",
-    "EOS",
-    "Elrond",
-    "Theta Token",
-    "Chiliz",
-    "Tezos",
-    "Pax Dollar",
-    "Paxos Standard",
-    "Bitcoin SV",
-    "SUSHI",
-    "Synthetix Network",
-    "Zcash",
-    "Curve DAO Token",
-    "ApeCoin",
-    "FTX Token",
-    "WOO Network",
-    "USDD",
-    "Maker",
-    "BitTorrent-New",
-    "IOTA",
-    "Fantom",
-    "KuCoin Token",
-    "Ravencoin",
-    "Axie Infinity",
-    "GateToken",
-    "eCash",
-    "Dash",
-    "NEO",
-    "Tokenize Xchange",
-    "WEMIX TOKEN",
-    "OEC Token",
-    "Bloktopia",
-    "Nexo",
-    "THORChain",
-    "PAX Gold",
-    "TrueUSD",
-    "Klaytn",
-    "BinaryX",
-    "Optimism",
-    "Ankr",
-    "Compound Ether",
-    "Compound Dai",
-    "Zilliqa",
-    "APENFT",
-    "Osmosis",
-    "Mina Protocol Token",
-    "Vulcan Forged",
-    "Celsius Network",
-    "Baby Doge Coin",
-    "Arweave",
+export const coins = [{
+    value: "Bitcoin",
+    label: "Bitcoin"
+},
+{
+    value: "Ethereum",
+    label: "Ethereum"
+},
+{
+    value: "Tether USD",
+    label: "Tether USD",
+},
+{
+    value: "USDC",
+    label: "USDC",
+},
+{
+    value: "BNB",
+    label: "BNB",
+},
+{
+    value: "XRP",
+    label: "XRP",
+},
+{
+    value: "Binance USD",
+    label: "Binance USD",
+},
+{
+    value: "Cardano",
+    label: "Cardano",
+},
+{
+    value: "Dogecoin",
+    label: "Dogecoin",
+},
+{
+    value: "OKB",
+    label: "OKB",
+},
+{
+    value: "Solana",
+    label: "Solana",
+},
+{
+    value: "Polygon",
+    label: "Polygon",
+},
+{
+    value: "Polkadot",
+    label: "Polkadot",
+},
+{
+    value: "Shiba Inu",
+    label: "Shiba Inu",
+},
+{
+    value: "Chainlink",
+    label: "Chainlink",
+},
+
+{
+    value: "Wrapped Ether",
+    label: "Wrapped Ether",
+},
+{
+    value: "Litecoin",
+    label: "Litecoin",
+},
+{
+    value: "TRON",
+    label: "TRON",
+},
+{
+    value: "Uniswap",
+    label: "Uniswap",
+},
+{
+    value: "Dai",
+    label: "Dai",
+},
+{
+    value: "Avalanche",
+    label: "Avalanche",
+},
+{
+    value: "Wrapped BTC",
+    label: "Wrapped BTC",
+},
+{
+    value: "Cosmos",
+    label: "Cosmos",
+},
+{
+    value: "Toncoin",
+    label: "Toncoin",
+},
+{
+    value: "Monero",
+    label: "Monero",
+},
+{
+    value: "Ethereum Classic",
+    label: "Ethereum Classic",
+},
+{
+    value: "Lido DAO Token",
+    label: "Lido DAO Token",
+},
+{
+    value: "Huobi Token",
+    label: "Huobi Token",
+},
+{
+    value: "Bitcoin Cash",
+    label: "Bitcoin Cash",
+},
+{
+    value: "Frax Share",
+    label: "Frax Share",
+},
+{
+    value: "Trust Wallet Token",
+    label: "Trust Wallet Token",
+},
+{
+    value: "Stellar",
+    label: "Stellar",
+},
+{
+    value: "Algorand",
+    label: "Algorand",
+},
+{
+    value: "Hedera",
+    label: "Hedera",
+},
+{
+    value: "Aptos",
+    label: "Aptos",
+},
+{
+    value: "Filecoin",
+    label: "Filecoin",
+},
+{
+    value: "The Sandbox",
+    label: "The Sandbox",
+},
+{
+    value: "Wrapped liquid staked Ether 2.0",
+    label: "Wrapped liquid staked Ether 2.0",
+},
+{
+    value: "Decentraland",
+    label: "Decentraland",
+},
+{
+    value: "Internet Computer (DFINITY)",
+    label: "Internet Computer (DFINITY)",
+},
+{
+    value: "NEAR Protocol",
+    label: "NEAR Protocol",
+},
+{
+    value: "VeChain",
+    label: "VeChain",
+},
+{
+    value: "EnergySwap",
+    label: "EnergySwap",
+},
+{
+    value: "Frax",
+    label: "Frax",
+},
+{
+    value: "Quant",
+    label: "Quant",
+},
+{
+    value: "PancakeSwap",
+    label: "PancakeSwap",
+},
+{
+    value: "Immutable X",
+    label: "Immutable X",
+},
+{
+    value: "Cronos",
+    label: "Cronos",
+},
+{
+    value: "Aave",
+    label: "Aave",
+},
+{
+    value: "Terra Classic",
+    label: "Terra Classic",
+},
+{
+    value: "Flow",
+    label: "Flow",
+},
+{
+    value: "EOS",
+    label: "EOS",
+},
+{
+    value: "Elrond",
+    label: "Elrond",
+},
+{
+    value: "Theta Token",
+    label: "Theta Token",
+},
+{
+    value: "Chiliz",
+    label: "Chiliz",
+},
+{
+    value: "Tezos",
+    label: "Tezos",
+},
+{
+    value: "Pax Dollar",
+    label: "Pax Dollar",
+},
+{
+    value: "Paxos Standard",
+    label: "Paxos Standard",
+},
+{
+    value: "Bitcoin SV",
+    label: "Bitcoin SV",
+},
+{
+    value: "SUSHI",
+    label: "SUSHI",
+},
+{
+    value: "Synthetix Network",
+    label: "Synthetix Network"
+},
+{
+    value: "Zcash",
+    label: "Zcash",
+},
+{
+    value: "Curve DAO Token",
+    label: "Curve DAO Token",
+},
+
+{
+    value: "ApeCoin",
+    label: "ApeCoin",
+},
+{
+    value: "FTX Token",
+    label: "FTX Token",
+},
+
+{
+    value: "WOO Network",
+    label: "WOO Network",
+},
+{
+    value: "USDD",
+    label: "USDD",
+},
+
+{
+    value: "Maker",
+    label: "Maker",
+},
+{
+    value: "BitTorrent-New",
+    label: "BitTorrent-New",
+},
+
+{
+    value: "IOTA",
+    label: "IOTA",
+},
+{
+    value: "Fantom",
+    label: "Fantom",
+},
+{
+    value: "KuCoin Token",
+    label: "KuCoin Token",
+},
+{
+    value: "Ravencoin",
+    label: "Ravencoin",
+},
+{
+    value: "Axie Infinity",
+    label: "Axie Infinity",
+},
+{
+    value: "GateToken",
+    label: "GateToken",
+},
+{
+    value: "eCash",
+    label: "eCash",
+},
+{
+    value: "Dash",
+    label: "Dash",
+},
+{
+    value: "NEO",
+    label: "NEO",
+},
+{
+    value: "Tokenize Xchange",
+    label: "Tokenize Xchange",
+},
+{
+    value: "WEMIX TOKEN",
+    label: "WEMIX TOKEN",
+},
+{
+    value: "OEC Token",
+    label: "OEC Token",
+},
+{
+    value: "Bloktopia",
+    label: "Bloktopia",
+},
+{
+    value: "Nexo",
+    label: "Nexo",
+},
+{
+    value: "THORChain",
+    label: "THORChain",
+},
+{
+    value: "PAX Gold",
+    label: "PAX Gold",
+},
+{
+    value: "TrueUSD",
+    label: "TrueUSD",
+},
+{
+    value: "Klaytn",
+    label: "Klaytn",
+},
+{
+    value: "BinaryX",
+    label: "BinaryX",
+},
+{
+    value: "Optimism",
+    label: "Optimism",
+},
+{
+    value: "Ankr",
+    label: "Ankr",
+},
+{
+    value: "Compound Ether",
+    label: "Compound Ether",
+},
+{
+    value: "Compound Dai",
+    label: "Compound Dai",
+},
+{
+    value: "Zilliqa",
+    label: "Zilliqa",
+},
+{
+    value: "APENFT",
+    label: "APENFT",
+},
+{
+    value: "Osmosis",
+    label: "Osmosis",
+},
+{
+    value: "Mina Protocol Token",
+    label: "Mina Protocol Token",
+},
+{
+    value: "Vulcan Forged",
+    label: "Vulcan Forged",
+},
+{
+    value: "Celsius Network",
+    label: "Celsius Network",
+},
+{
+    value: "Baby Doge Coin",
+    label: "Baby Doge Coin",
+},
+{
+    value: "Arweave",
+    label: "Arweave",
+}
 ];
