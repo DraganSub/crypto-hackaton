bf294b0d86d49698cad7c0f6ab36b352a63c9dab
dsubotic <dragan.subi7@gmail.com>
2023-01-27 00:31:51 +0100

    add skeleton loader dark theme

--- src/components/skeleton-loaders/NewsSkeletonLoader.tsx
+++ src/components/skeleton-loaders/NewsSkeletonLoader.tsx
@@ -1,29 +1,37 @@
 import { Skeleton } from "antd";
 import Card from "antd/es/card/Card";
+import classNames from "classnames";
+import { useTheme } from "../../hooks";
 
 export const NewsSkeletonLoader = () => {
   // dummy data for fixed 20 cards display 
+  const { theme } = useTheme();
   let newsSkeletonData = [];
   for (let i = 0; i < 20; i++) {
     newsSkeletonData.push(i)
   }
+
+  const cryptoCardClass = classNames("crypto_news-card", {
+    "card-is--dark": theme === "dark"
+  });
 
   return (
     <>
       {newsSkeletonData.map((item) => {
         return (
           <div key={item}>
-            <Card className="crypto_news-card">
-              <div className="crypto__card__title">
-                <h5><Skeleton.Input active={true} size={"small"} /></h5>
-                <div>
-                  <Skeleton.Avatar active={true} size={"small"} />
+            <Card className={cryptoCardClass}>
+              <div className="crypto__card__title" style={{ padding: "10px" }}>
+                <h5 ><Skeleton.Input active={true} size={"small"} /></h5>
+                <div style={{ padding: "10px" }}>
+                  <Skeleton.Avatar active={true} size={"large"} />
                 </div>
               </div>
               <Skeleton.Input active={true} size={"small"} />
-              <div className="news-info">
-                <div className="news-provider"><Skeleton.Input active={true} size={"small"} /></div>
-                <div className="news-time"><Skeleton.Input active={true} size={"small"} /></div>
+              <div className="news-info" style={{ padding: "10px" }}>
+                <div className="news-provider" style={{ padding: "10px" }}><Skeleton.Input active={true} size={"small"} /></div>
+
+                <div className="news-time" style={{ padding: "10px" }}><Skeleton.Input active={true} size={"small"} /></div>
               </div>
             </Card>
           </div>
